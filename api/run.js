3057a80cce11f7960a26be63500d7fbf04a9ffb0
Paul Kinlan <paul.kinlan@gmail.com>
2021-05-04 15:47:15 +0100

    formatting

--- api/run.js
+++ api/run.js
@@ -1,4 +1,29 @@
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
-}
+import { run } from "./squoosh/index.js";
+
+module.exports = async (req, res) => {
+  const {
+    body = undefined,
+    suffix = "",
+    optimizerButteraugliTarget = false,
+    outputDir,
+    maxOptimizerRounds,
+  } = req.query;
+
+  /*
+  files = [],
+  suffix = '',
+  optimizerButteraugliTarget = false,
+  outputDir = '',
+  maxOptimizerRounds = 8,
+  ...extras
+  */
+
+  const files = [];
+  return await run({
+    files,
+    suffix,
+    optimizerButteraugliTarget,
+    outputDir,
+    maxOptimizerRounds,
+  });
+};
