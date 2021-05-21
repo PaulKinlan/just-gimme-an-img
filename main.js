84d7c7091b947e800ff6bfd6b3d800352b3cf97a
Paul Kinlan <paul.kinlan@gmail.com>
2021-05-21 10:19:48 +0100

    Adding Web Vitals

--- main.js
+++ main.js
@@ -16,6 +16,33 @@ import {
 
 import { App, ImageTask } from "./lib/app";
 
+import {getCLS, getFID, getLCP} from 'web-vitals';
+
+function sendToGoogleAnalytics({name, delta, value, id}) {
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/ga4
+  gtag('event', name, {
+    // Built-in params:
+    value: delta, // Use `delta` so the value can be summed.
+    // Custom params:
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+
+    // OPTIONAL: any additional params or debug info here.
+    // See: https://web.dev/debug-web-vitals-in-the-field/
+    // metric_rating: 'good' | 'ni' | 'poor',
+    // debug_info: '...',
+    // ...
+  });
+}
+
+window.addEventListener("load", () => {
+  getCLS(sendToGoogleAnalytics);
+  getFID(sendToGoogleAnalytics);
+  getLCP(sendToGoogleAnalytics);
+});
+
 const updateSW = registerSW({
   onNeedRefresh() {
     // show a prompt to user
